f3162d3ad25c832832cc2909573ad34a7c9c7c27
EmersonGald1n0 <rm93899@fiap.com.br>
2022-09-14 08:54:54 -0300

    Comp lista tarefas criada

---
+++ rm93899/react-aula7/src/components/listaTarefas/ListaTarefas.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import React from 'react';
+
+
+export default function listaTarefas() {
+  return (
+    <div>
+
+      <p>Lista de Tarefas</p>
+
+    </div>
+  )
+}
