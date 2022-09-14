20deee9134324f02c68af8cebbb243bedbd320da
RafaRamosCosta <rm94281@fiap.com.br>
2022-09-14 09:16:54 -0300

    Crei comp tarefa e o módulo de estilo DivTarefa.

---
+++ rm94281/react-aula7/src/components/tarefa/Tarefa.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const DivTarefa = styled.div`
+  background-color: #ffb;
+  border: 2px solid #333;
+  box-shadow: 5px 5px 5px #333;
+  padding: 20px;
+  text-align: center;
+  width: 350px;
+  height: 200px;
+  margin: 10px;
+  h2,
+  p {
+    padding-bottom: 10px;
+  }
+`;
+
+export default function Tarefa() {
+  return (
+    <DivTarefa>
+      <h2>Relatório de Vendas</h2>
+      <p>Para: Departamento de Vendas</p>
+      <p>Levantar os valores das vendas deste mês.</p>
+    </DivTarefa>
+  );
+}
