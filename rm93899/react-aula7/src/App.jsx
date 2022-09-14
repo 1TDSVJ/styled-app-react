9a28572e5a4f0f29b49b8e0d59008eb983888ee4
EmersonGald1n0 <rm93899@fiap.com.br>
2022-09-14 08:36:18 -0300

    Boyler plate react + styled comp.

---
+++ rm93899/react-aula7/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+import React from 'react';
+
+
+
+export default function App() {
+  return (
+    <div>
+      <h1>ESTUDO COM STYLED-COMPONENTS</h1>
+    </div>
+  )
+}
+
+
