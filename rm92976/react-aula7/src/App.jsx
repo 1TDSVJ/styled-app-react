0634f3452714e360ea250679a7291cf90bc601af
heitorborbamarini <rm92976@fiap.com.br>
2022-09-14 08:53:09 -0300

    começo do projeto

---
+++ rm92976/react-aula7/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import ListaTarefas.jsx from './components/listaTarefas';
+
+export default function App(){
+    return(
+        <div>
+            <h1>Estudo com STYLED-COMPONENTS</h1>
+        </div>
+    )
+}
