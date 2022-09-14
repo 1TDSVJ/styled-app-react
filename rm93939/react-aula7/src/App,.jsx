41e38ec1e6920caada896433669b1bce8fbb979e
PedroChueiri <pedrochu99@hotmail.com>
2022-09-14 08:57:02 -0300

    import ListaTarefas In App.jsx

--- rm93939/react-aula7/src/App,.jsx
+++ rm93939/react-aula7/src/App,.jsx
@@ -1,1 +1,13 @@
 import React from "react"
+import ListaTarefas from "./components/listaTarefas/ListaTarefas";
+
+const App =()=> {
+  return (
+    <div>
+      <h1>ESTUDO COM STULED_COMPONENTS</h1>
+      <ListaTarefas />
+    </div>
+  );
+}
+
+export default App
