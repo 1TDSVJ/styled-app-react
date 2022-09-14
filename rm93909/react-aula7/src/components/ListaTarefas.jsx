163b79079e176b25ae9027ae72488aaab49bc436
lumitsudo <rm93909@fiap.com.br>
2022-09-14 09:41:49 -0300

    useState

--- rm93909/react-aula7/src/components/ListaTarefas.jsx
+++ rm93909/react-aula7/src/components/ListaTarefas.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Tarefa from './Tarefa'
+import { DivLista } from '../style/styled'
 
 export default function ListaTarefas() {
-  return (
-    <div>
-        <Tarefa/>
-        <Tarefa/>
-        <Tarefa/>
-    </div>
-  )
+
+    const [tarefa, setTarefa]= useState([{
+        titulo: 'Lista de Pagamento',
+        setor: 'Dep. Vendas',
+        descricao: 'Levantar os valores das vendas',
+      },
+      {
+        titulo: 'Planilha de Salários',
+        setor: 'Dep. Pessoal',
+        descricao: 'Gerar planilhas',
+      },
+    ])
+
+    return (
+    <DivLista>
+        {tarefa.map((tar, i)=>(
+            <Tarefa
+              key={i}
+              titulo={tar.titulo}
+              setor={tar.setor}
+              descricao={tar.descricao}
+            />
+        ))}        
+    </DivLista>
+    )
 }
--- rm93909/react-aula7/src/components/Tarefa.jsx
+++ rm93909/react-aula7/src/components/Tarefa.jsx
@@ -1,25 +1,12 @@
 import React from 'react'
-import styled from 'styled-components'
+import { DivTarefa } from '../style/styled'
 
-const DivTarefa = styled.div`
-    background-color: #ffb;
-    border: 2px solid #333;
-    box-shadow: 5px 5px 5px #333;
-    padding: 20px;
-    text-align: center;
-    width: 350px;
-    height: 200px;
-    margin: 10px;
-    h2,p{ padding-bottom: 10px; }
-`
-
-
-export default function Tarefa() {
+export default function Tarefa(props) {
   return (
     <DivTarefa>
-        <h2>Relatório de vendas</h2>
-        <p>Para: Departamento de Vendas</p>
-        <p>Levantar os valores das vendas deste mês.</p>
+        <h2>{props.titulo}</h2>
+        <p>Para: {props.setor}</p>
+        <p>{props.descricao}</p>
     </DivTarefa>
   )
 }
