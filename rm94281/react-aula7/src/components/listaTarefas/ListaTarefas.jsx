7ae4e75e8ecfec57826e40829417b97020526b58
RafaRamosCosta <rm94281@fiap.com.br>
2022-09-14 09:48:21 -0300

    Criei função para adicionar uma tarefa

--- rm94281/react-aula7/src/components/listaTarefas/ListaTarefas.jsx
+++ rm94281/react-aula7/src/components/listaTarefas/ListaTarefas.jsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Tarefa from '../tarefa/Tarefa';
-
+import { DivLista } from '../../style/styled';
 export default function ListaTarefas() {
+  const [tarefa, setTarefa] = useState([
+    {
+      titulo: 'Lista de Pagamento',
+      setor: 'Dep. Vendas',
+      descricao: 'Levantar os valores das vendas',
+    },
+    {
+      titulo: 'Planilha de Salários',
+      setor: 'Dep. Pessoal',
+      descricao: 'Gerar planilhas',
+    },
+  ]);
+
+  const addTarefa = () => {
+    const novaTarefa = {
+      titulo: 'Planilha de Salários',
+      setor: 'Dep. Pessoal',
+      descricao: 'Gerar planilhas',
+    };
+
+    setTarefa([...tarefa, novaTarefa]);
+  };
   return (
-    <div>
-      <Tarefa />
-      <Tarefa />
-      <Tarefa />
-    </div>
+    <DivLista>
+      {tarefa.map((t, i) => (
+        <Tarefa
+          key={i}
+          titulo={t.titulo}
+          setor={t.setor}
+          descricao={t.descricao}
+        />
+      ))}
+      <button onClick={addTarefa}>Adicionar Tarefa</button>
+    </DivLista>
   );
 }
