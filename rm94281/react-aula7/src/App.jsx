9b07d182348f1f85475ef555babcc41e9fd581eb
RafaRamosCosta <rm94281@fiap.com.br>
2022-09-14 09:48:41 -0300

    renderizando os componentes no App e o App no index.js

--- rm94281/react-aula7/src/App.jsx
+++ rm94281/react-aula7/src/App.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import ListaTarefas from './components/listaTarefas/ListaTarefas';
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>Estudo com styled components</h1>
+      <ListaTarefas />
+    </div>
+  );
 }
