942fdf69d253c95c12391d03c0fc3ddc7a5348df
LeonardoRCosta <rm94286@fiap.com.br>
2022-09-14 09:26:55 -0300

    Importado o módulo DivLista.

--- rm94286/react-aula7/src/components/listaTarefas/ListaTarefas.jsx
+++ rm94286/react-aula7/src/components/listaTarefas/ListaTarefas.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Tarefa from '../tarefa/Tarefa'
+import { DivLista } from '../../style/styled'
 
 export default function ListaTarefas() {
   return (
-    <div>
+    <DivLista>
         <Tarefa />
         <Tarefa />
         <Tarefa />
-    </div>
+    </DivLista>
   )
 }
