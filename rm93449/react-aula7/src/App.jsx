c0402342e7f950c0389097d3f9c71ce925c36981
GuWuelta <rm93449@fiap.com.br>
2022-09-14 08:36:21 -0300

    Boyler plate react + styld comp.

---
+++ rm93449/react-aula7/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+    return(
+        <div>
+            <h1>ESTUDO COM STYLED COMPONENTS</h1>
+        </div>
+    );
+}
+
+export default App;
