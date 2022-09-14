089d0b0235d027521b6f9da5b2c2e09f977c3109
LeonardoRCosta <rm94286@fiap.com.br>
2022-09-14 09:16:54 -0300

    Componente Tarefa importado.

--- rm94286/react-aula7/src/components/listaTarefas/ListaTarefas.jsx
+++ rm94286/react-aula7/src/components/listaTarefas/ListaTarefas.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Tarefa from '../tarefa/Tarefa'
 
 export default function ListaTarefas() {
   return (
     <div>
-        <p>Lista de Tarefas</p>
+        <Tarefa />
+        <Tarefa />
+        <Tarefa />
     </div>
   )
 }
