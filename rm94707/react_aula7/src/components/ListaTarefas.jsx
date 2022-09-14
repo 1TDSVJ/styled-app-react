3c60a3091ee7b892fcc0b264dc599f01b120c197
Vinicius Reis <rm94707@fiap.com.br>
2022-09-14 09:49:14 -0300

    feito style.js

--- rm94707/react_aula7/src/components/ListaTarefas.jsx
+++ rm94707/react_aula7/src/components/ListaTarefas.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
+import { DivLista } from '../style/styled'
 import Tarefa from './Tarefa'
-
-
 
 export default function ListaTarefas() {
   return (
-    <div>
+    <DivLista>
         <Tarefa/>
         <Tarefa/>
         <Tarefa/>
-    </div>
+    </DivLista>
   )
 }
