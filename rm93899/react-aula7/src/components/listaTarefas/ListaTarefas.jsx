8438e41785b33dd5b2536e9d343d781f0cab15c6
EmersonGald1n0 <rm93899@fiap.com.br>
2022-09-14 09:39:20 -0300

    criado comp style.js e importado nos respectivos comp

--- rm93899/react-aula7/src/components/listaTarefas/ListaTarefas.jsx
+++ rm93899/react-aula7/src/components/listaTarefas/ListaTarefas.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import { DivLista } from '../../style/styled'
 import Tarefa from '../tarefa/Tarefa'
 
 
 export default function listaTarefas() {
   return (
-    <div>
+    <DivLista>
 
       <Tarefa/>
       <Tarefa/>
       <Tarefa/>
 
-    </div>
+    </DivLista>
   )
 }
