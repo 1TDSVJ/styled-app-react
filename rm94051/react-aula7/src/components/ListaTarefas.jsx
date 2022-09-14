b0579b01476008142c2ac054751621ed4fc0abf2
LuisFelipeGM <lipe.garcia.menezes@gmail.com>
2022-09-14 09:39:45 -0300

    Add StyledComponents na pasta Style, começo tarefa

--- rm94051/react-aula7/src/components/ListaTarefas.jsx
+++ rm94051/react-aula7/src/components/ListaTarefas.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { DivLista } from '../style/styled'
 import Tarefa from './Tarefa'
 
 export default function ListaTarefas() {
+
+    const [tarefa, setTarefa] = useState([{
+            titulo:"Lista de Pagamento",
+            setor:"Dep. Vendas",
+            descricao: "Levantar os valores das vendas",
+        },
+        {
+            titulo:"Planilha de Sálarios",
+            setor:"Dep. Pessoal",
+            descricao: "Gerar planilhas",
+        }
+    ])
+
   return (
-    <div>
-
-        <Tarefa/>
-        <Tarefa/>
-        <Tarefa/>
-
-    </div>
+    <DivLista>
+        {tarefa.map((tar, i)=>(
+            <Tarefa
+                key={i}
+                titulo={tar.titulo}
+                setor={tar.setor}
+                descricao={tar.descricao}
+            />
+        ))}
+    </DivLista>
   )
 }
