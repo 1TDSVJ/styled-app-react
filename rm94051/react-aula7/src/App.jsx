f78efeb119f5c03e8b100896119a5a8674ac1f73
LuisFelipeGM <lipe.garcia.menezes@gmail.com>
2022-09-14 09:00:56 -0300

    add ListaTarefas

--- rm94051/react-aula7/src/App.jsx
+++ rm94051/react-aula7/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import ListaTarefas from './components/ListaTarefas'
 
 export default function App(){
     return(
         <div>
             <h1>ESTUDO COM STYLED-COMPONENTS</h1>
+            <ListaTarefas/>
         </div>
     )
 }
