6864588388b738f1d3b5ad080763ef8e169b9866
LeonardoRCosta <rm94286@fiap.com.br>
2022-09-14 08:58:26 -0300

    Componente ListaTarefas importado.

--- rm94286/react-aula7/src/App.jsx
+++ rm94286/react-aula7/src/App.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from 'react';
+import ListaTarefas from './components/listaTarefas/ListaTarefas';
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <div>
+      <h1>Estudo com Styled-Components</h1>
+      <ListaTarefas />
+    </div>
+  );
 }
