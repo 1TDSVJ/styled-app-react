4f559b8b9b3d8793e9b586fdbfc4d7e09f8993c1
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-09-14 09:42:48 -0300

    Criado arquivo de modulos.

--- pf0670/react-aula7/src/components/ListaTarefas.jsx
+++ pf0670/react-aula7/src/components/ListaTarefas.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import { DivLista } from '../style/styled'
 import Tarefa from './Tarefa'
 
 export default function ListaTarefas() {
   return (
-    <div>
+    <DivLista>
 
         <Tarefa/>
         <Tarefa/>
         <Tarefa/>
 
-    </div>
+    </DivLista>
   )
 }
