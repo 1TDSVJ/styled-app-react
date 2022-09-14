4ecdc92c3428c08aa728d9aa7e8ffcddfa3a1fea
lumitsudo <rm93909@fiap.com.br>
2022-09-14 09:16:59 -0300

    Criei comp. Tarefa e ListaTarefas

---
+++ rm93909/react-aula7/src/components/ListaTarefas.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Tarefa from './Tarefa'
+
+export default function ListaTarefas() {
+  return (
+    <div>
+        <Tarefa/>
+        <Tarefa/>
+        <Tarefa/>
+    </div>
+  )
+}
