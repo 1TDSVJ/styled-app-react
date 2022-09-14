c425e8e0f32746067a999f4a84d130a543f42a40
PedroChueiri <pedrochu99@hotmail.com>
2022-09-14 09:36:02 -0300

    finalizado agr fazendo o exercicio

--- rm93939/react-aula7/src/components/listaTarefas/ListaTarefas.jsx
+++ rm93939/react-aula7/src/components/listaTarefas/ListaTarefas.jsx
@@ -1,13 +1,25 @@
-import React from "react"
+import React, { useState } from "react"
+import { DivLista } from "../../style/styled"
 import Tarefa from "../terefa/Tarefa"
 
 const ListaTarefas = () => {
+
+    const [tarefa,setTarefa]= useState([{
+        titulo: 'lista de pagamento',
+        setor: 'Dep. Vendas',
+        descricao: 'Levantar os valores das vendas',  
+        },
+        {
+        titulo: 'Planilha de Salario',
+        setor: 'Dep. Pessoal',
+        descricao: 'Gerar planilhas',
+        },
+    ])
+    
     return(
-        <div>
-            <Tarefa />
-            <Tarefa />
-            <Tarefa />
-        </div>
+        <DivLista>
+            
+        </DivLista>
     )
 }
 
--- rm93939/react-aula7/src/components/terefa/Tarefa.jsx
+++ rm93939/react-aula7/src/components/terefa/Tarefa.jsx
@@ -1,17 +1,5 @@
 import React from "react"
-import styled from 'styled-components';
-
-const DivTarefa = styled.div`
-  background-color: #ffb;
-  border: 2px solid #333;
-  box-shadow: 5px 5px 5px #333;
-  padding: 20px;
-  text-align: center;
-  width: 350px;
-  height: 200px;
-  margin: 10px;
-  h2,p{ padding-bottom: 10px;}
-`
+import { DivTarefa } from "../../style/styled";
 
 
 const Tarefa = ()=>{
