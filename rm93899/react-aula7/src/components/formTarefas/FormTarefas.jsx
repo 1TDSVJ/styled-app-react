be8ccbed8a3af329992bc5ede2be1f4f14f41869
EmersonGald1n0 <rm93899@fiap.com.br>
2022-09-15 02:17:56 -0300

    add estilização p/ formulário

--- rm93899/react-aula7/src/components/formTarefas/FormTarefas.jsx
+++ rm93899/react-aula7/src/components/formTarefas/FormTarefas.jsx
@@ -1,4 +1,15 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const DivForm = styled.div`
+    width: 100%;
+    padding: 20px;
+    background-color: #993;
+    form{width:50%; margin: auto;}
+    form input, form textarea{padding:5px; width:80%;}
+    form textarea{height: 80px;}
+    form button{padding:10px; background: #3c3; color:#fff;}
+`
 
 export default function FormTarefas() {
   
