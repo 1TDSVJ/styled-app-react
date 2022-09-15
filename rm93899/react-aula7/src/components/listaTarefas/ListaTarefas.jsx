ee4534f7c11be08936bee24ef972246df3d61b13
EmersonGald1n0 <rm93899@fiap.com.br>
2022-09-15 01:37:17 -0300

    import useState p/ guardar os dados das taref+ map

--- rm93899/react-aula7/src/components/listaTarefas/ListaTarefas.jsx
+++ rm93899/react-aula7/src/components/listaTarefas/ListaTarefas.jsx
@@ -1,16 +1,33 @@
 import React from 'react'
 import { DivLista } from '../../style/styled'
 import Tarefa from '../tarefa/Tarefa'
+import { useState } from 'react'
 
 
 export default function listaTarefas() {
+  
+  const [tarefas, setTarefas] = useState([{
+    titulo: 'Lista de Pagamento',
+    setor: 'Dep. Vendas',
+    descricao: 'Levantar os valores de vendas.',
+  },
+  {
+    titulo: 'Planilha de Salários',
+    setor: 'Dep. Pessoal',
+    descricao: 'Gerar planilhas',
+  },
+])
+  
   return (
     <DivLista>
-
-      <Tarefa/>
-      <Tarefa/>
-      <Tarefa/>
-
+      {tarefas.map((tar, i) => (
+        <Tarefa
+          key={i}
+          titulo={tar.titulo}
+          setor={tar.setor}
+          descricao={tar.descricao}
+          />
+      ))}
     </DivLista>
   )
 }
