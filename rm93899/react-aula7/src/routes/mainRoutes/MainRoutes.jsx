91f2728063dfca263e38d8d6065a2c68c4793398
EmersonGald1n0 <rm93899@fiap.com.br>
2022-09-15 02:31:21 -0300

    montando as rotas

---
+++ rm93899/react-aula7/src/routes/mainRoutes/MainRoutes.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import { Route, Routes } from "react-router-dom"
+import FormTarefas from "../components/FormTarefas"
+import Home from "../components/Home"
+import ListaTarefas from "../components/ListaTarefas"
+
+export default function MainRoutes() {
+
+    return(
+        <>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/lt" element={<ListaTarefas/>}/>
+                <Route path="/add" element={<FormTarefas/>}/>
+            </Routes>
+        </>
+    )
+}
