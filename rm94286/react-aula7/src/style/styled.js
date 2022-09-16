bcb80b42121a836e7a9a4b96b65d004fcb42a1e7
LeonardoRCosta <rm94286@fiap.com.br>
2022-09-16 17:08:34 -0300

    Estilização dos botões.

--- rm94286/react-aula7/src/style/styled.js
+++ rm94286/react-aula7/src/style/styled.js
@@ -1,13 +1,23 @@
 import styled from 'styled-components';
 
 export const DivLista = styled.div`
-    width: 100%;
-    padding: 20px;
-    min-height: 85vh;
-    background-color: #ccc;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-around;
+  width: 100%;
+  padding: 20px;
+  min-height: 85vh;
+  background-color: #ccc;
+  display: flex; flex-wrap: wrap;
+  justify-content: space-around;
+
+  .btnTarefa {
+    display: block;
+    height: 8vh;
+    margin: auto;
+    border-radius: 5px;
+    padding: 1vh 2vw;
+    border: solid 2px #00ff00;
+    color: #00ff00;
+    font-size: 1.5rem;
+  }
 `;
 
 export const DivTarefa = styled.div`
@@ -23,4 +33,14 @@ export const DivTarefa = styled.div`
   p {
     padding-bottom: 10px;
   }
+`;
+
+export const Botao = styled.button`
+  border-radius: 5px;
+  padding: 1vh 2vw;
+  margin: auto;
+  display: block;
+  border: solid 2px #00ff00;
+  color: #00ff00;
+  font-size: 1.5rem;
 `;
